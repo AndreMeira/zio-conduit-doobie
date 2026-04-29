a55319dc380d60b5e1e6ca56fa7aae2ef24eab7e
Andre Meira <a.meira92@gmail.com>
2026-04-29 20:26:40 +0200

    type parameter rename

--- src/main/scala/conduit/domain/logic/authorisation/definition/Authorisation.scala
+++ src/main/scala/conduit/domain/logic/authorisation/definition/Authorisation.scala
@@ -4,11 +4,11 @@ import conduit.domain.model.error.ApplicationError
 import conduit.domain.model.error.ApplicationError.UnauthorisedError
 import zio.ZIO
 
-trait Authorisation[Tx, A, E <: UnauthorisedError] {
+trait Authorisation[Tx, Request, Failure <: UnauthorisedError] {
   type Error <: ApplicationError
-  protected type Result = ZIO[Tx, Error, Authorisation.Result[E]]
+  protected type Result = ZIO[Tx, Error, Authorisation.Result[Failure]]
 
-  def authorise(request: A): Result
+  def authorise(request: Request): Result
 
   // Helper method when no logic is needed
   protected def allowed: Result = ZIO.succeed(Authorisation.Result.Allowed)
